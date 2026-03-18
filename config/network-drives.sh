5fb1c33d180e838ec50c9ff0987c6e9b78fe61f9
Alex Mihai <alexmihai1984@gmail.com>
2026-03-18 23:55:01 +0200

    network drives

--- config/network-drives.sh
+++ config/network-drives.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo mkdir --parents /media/external/ssd1
+sudo mkdir --parents /media/external/ssd2
+sudo mkdir --parents /media/external/nas
+
 echo "//192.168.68.112/ssd1    /media/external/ssd1  cifs  credentials=/home/alex/.smbcredentials,uid=1000,mfsymlinks,iocharset=utf8,file_mode=0777,dir_mode=0777,sec=ntlmssp,_netdev,nofail  0  0" | sudo tee -a /etc/fstab
 echo "//192.168.68.112/ssd2    /media/external/ssd2  cifs  credentials=/home/alex/.smbcredentials,uid=1000,mfsymlinks,iocharset=utf8,file_mode=0777,dir_mode=0777,sec=ntlmssp,_netdev,nofail  0  0" | sudo tee -a /etc/fstab
 echo "//192.168.68.112/data    /media/external/nas   cifs  credentials=/home/alex/.smbcredentials,uid=1000,mfsymlinks,iocharset=utf8,file_mode=0777,dir_mode=0777,sec=ntlmssp,_netdev,nofail  0  0" | sudo tee -a /etc/fstab
