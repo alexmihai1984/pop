f0cda076a7b7e15961ce0f977ab096dc1736ee48
Alex Mihai <alexmihai1984@gmail.com>
2026-03-19 22:53:48 +0200

    eza

--- install-all.sh
+++ install-all.sh
@@ -3,6 +3,7 @@
 ./install/install-antigravity.sh
 ./install/install-btop.sh
 ./install/install-cifs.sh
+./install/install-eza.sh
 ./install/install-fzf.sh
 ./install/install-mtr.sh
 ./install/install-pipx.sh
